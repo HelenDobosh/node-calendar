2dede2165f70b6dd50ad1004eeccdefd0fe633f5
Olena Dobosh <o.dobosh@relevant.software>
2020-10-31 20:40:57 +0200

    [HT-3] - added get events-batch, README, moved functions to utils

--- events-api-ht-3/events.js
+++ events-api-ht-3/events.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const fs = require('fs');
 const bodyParser = require('body-parser');
+
+
 const app = express();
 const PORT = 3000;
-const csvFile = "events.csv";
+
+
+const {
+  readFile,
+  getEventsByLocation,
+  getEventById,
+  addNewEvent,
+  updateEvent,
+  getEventsBatch
+} = require('./utils');
 
 app.use(bodyParser.json());
 
@@ -20,93 +30,39 @@ app.get('/events/:eventId', async (req, res) => {
 
   const eventById = await getEventById(eventId);
 
-  res.json(eventById);
+  if (eventById) {
+    res.json(eventById);
+  } else {
+    res.status(404).send(`Event with id:${eventId} doesn't exist.`);
+  }
 });
 
 app.post('/events', async (req, res) => {
-  await writeEventToEventsFile(req.body);
-  res.sendStatus(200);
+  await addNewEvent(req.body);
+  res.sendStatus(201);
 });
 
-function readFile(filePath = csvFile, parseToJson = true, encoding = 'utf8') {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, encoding, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(parseToJson? parseCsvToJson(data) : data);
-      }
-    });
-  });
-}
+app.put('/events/:eventId', async (req, res) => {
+  const {eventId} = req.params;
 
-async function getEventsByLocation(location) {
-  const fileContent = await readFile();
-  return location ? fileContent.filter(event => event.location.toLowerCase() === location.toLowerCase()) : fileContent;
-}
+  const events = await readFile();
 
-async function getEventById(id) {
-  const fileContent = await readFile();
-  return id ? fileContent.filter(event => event.id === id) : fileContent;
-}
+  const event = events.find(event => event.id === eventId);
 
-function writeEventToEventsFile(reqBody) {
-  const id = new Date().getTime();
+  if (!event) {
+    return res.status(404).send(`Event with id:${eventId} doesn't exist.`);
+  } else {
+    const event = await updateEvent(eventId, events, req.body);
 
-  let logStream = fs.createWriteStream(csvFile, {flags: 'a'});
+    res.status(200).send(event);
+  }
+});
 
-  const newEvent = parseJsonToCsv(reqBody);
-  newEvent.unshift(id);
-  logStream.write(`${newEvent}\n`);
-
-  logStream.end();
-}
-
-
-function parseCsvToJson(csvData) {
-  const fileArr = csvData.toString()
-    .split('\n')
-    .filter(str => str.length > 0);
-
-  const columnNamesArr = fileArr[0].split(',');
-
-  const fileData = fileArr.slice(1);
-
-  return fileData.map(row => {
-    return row.split(',').reduce((acc, item, index) => {
-      acc[columnNamesArr[index]] = item;
-      return acc;
-    }, {})
-  });
-}
-
-function parseJsonToCsv(jsonData) {
-  return Object.values(jsonData);
-}
+app.get('/events-batch', async (req, res) => {
+  await getEventsBatch(res);
+});
 
 
 app.listen(PORT, () => {
-  console.log(`server start at port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
-
-
-// Create GET /events?location=lviv endpoint which returns events from csv file in json format.
-// It should support possible filtering events by location (passed as query parameter).​
-//
-// Create GET /events/:eventId endpoint for getting some specific event by id.​
-//
-// Create POST /events endpoint for saving new event to the csv file.​
-
-
-
-//
-// Create PUT /events/:eventId endpoint for replacing specific event data in csv file.​
-//
-// Create GET /events-batch endpoint which returns all events in json format via streaming directly from csv file.​
-//
-// ​
-//
-// Feel free to use any libraries you want.​
-//
-// Feel free to structure your code in any way (possibly with many files/modules). The easier it is to read the better.​
-// Please, add instructions how to properly test your code (how to start a server, CURL examples).​
